e6dcf19767f84b3e08bb6cabbda9d617c0669836
javigonzalo <javigonzauva@gmail.com>
2018-11-19 15:10:23 +0100

    First commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='quat_to_euler',
+      version='2018.11.19',
+      description='Adds a quaternion to Euler Unity-like convention method to NumPy',
+      url='http://github.com/',
+      author='Javier González',
+      author_email='javigonzauva@gmail.com',
+      license='MIT',
+      packages=['quat_to_euler'],
+      zip_safe=False)
